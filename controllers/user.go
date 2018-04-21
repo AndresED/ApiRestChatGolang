b3ed8f2760a437cf4668270851f1e5dc78542a08
AndresED <andres30xed@gmail.com>
2018-04-21 01:04:28 -0500

    Controlador User.go creado en la cual se ha implementado el login y el registro de usuario

---
+++ controllers/user.go
@@ -0,0 +1,84 @@
+package controllers
+
+import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/AndresED/api/commons"
+	"github.com/AndresED/api/configuration"
+	"github.com/AndresED/api/models"
+)
+
+// Login Inicio de sesion
+func Login(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	fmt.Println(r.Body)
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Error:%s", err)
+	}
+	db := configuration.GetConnection()
+	defer db.Close()
+	c := sha256.Sum256([]byte(user.Password))
+	pwd := base64.URLEncoding.EncodeToString(c[:32])
+	db.Where("email = ? and password=?", user.Email, pwd).First(&user)
+	if user.ID > 0 {
+		user.Password = ""
+		token := commons.GenerateJWT(user)
+		j, err := json.Marshal(models.Token{Token: token})
+
+		if err != nil {
+			log.Fatalf("Error al convertir el token a json")
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	} else {
+		m := models.Message{
+			Message: "Usuario o clave no válida",
+			Code:    http.StatusUnauthorized,
+		}
+		commons.DisplayMessage(w, m)
+	}
+}
+
+//UserCreate permite registrar un usuario
+func UserCreate(w http.ResponseWriter, r *http.Request) {
+	user := models.User{}
+	m := models.Message{}
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		m.Message = fmt.Sprintf("Error al leer el usuario a registrar: %s", err)
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+	if user.Password != user.ConfirmPassword {
+		m.Message = "Las contraseñas no coinciden"
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+	c := sha256.Sum256([]byte(user.Password))
+	pwd := fmt.Sprintf("%s", c)
+	user.Password = pwd
+	picmd5 := md5.Sum([]byte(user.Email))
+	picstr := fmt.Sprintf("%s", picmd5)
+	user.Picture = "https://gravatar.com/avatar/" + picstr + "?s=100"
+	db := configuration.GetConnection()
+	defer db.Close()
+	err = db.Create(&user).Error
+	if err != nil {
+		m.Message = fmt.Sprintf("Error al registrar: %s", err)
+		m.Code = http.StatusBadRequest
+		commons.DisplayMessage(w, m)
+		return
+	}
+	m.Message = "Usuario creado exitosamente"
+	m.Code = http.StatusCreated
+	commons.DisplayMessage(w, m)
+}
